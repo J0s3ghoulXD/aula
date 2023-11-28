c2443052f67a5a91ee47418534392a176be3e071
jose diaz ch <planfuego@hotmail.com>
2023-11-28 00:52:53 -0600

    ayudaaaaa

--- proyect/Form1.Designer.cs
+++ proyect/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(270, 238);
+            this.label1.Location = new System.Drawing.Point(228, 242);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 1;
